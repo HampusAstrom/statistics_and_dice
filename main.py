22c58aa0e2873521cd4452e899f5d7f10d13f8fa
Hampus Åström <hampus.thep@gmail.com>
2019-12-29 00:25:06 +0100

    Nicer plot of real and sampled dist, and some dice dist plots

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ def estimate_succ_rate(x, dist):
             succ += den
     return succ/(fail + succ)
 
+# maybe mu is defined by difference (attr + skill - difficulty)
+# how is "real" sigma defined?
+# sample size can be determined by (insight + skill)
+
 mu = 3
 #variance = 30
-sigma = 10
-samples = 10
+sigma = 5
+samples = 5
 
 
 #sigma = math.sqrt(variance)
@@ -38,9 +42,44 @@ print(sum(dist)*(stop-start)/len(x))
 succ_rate_samp = estimate_succ_rate(x, dist2)
 succ_rate_real = estimate_succ_rate(x, dist)
 
-print("Probability to succed estimated to: {}".format(succ_rate_samp))
-print("Real success probability:           {}".format(succ_rate_real))
+print("Probability to succeed estimated to: {}".format(succ_rate_samp))
+print("Real success probability:            {}".format(succ_rate_real))
 
-plt.plot(x, dist)
-plt.plot(x, dist2)
+fig, ax = plt.subplots()
+
+ymax = 0.2
+
+ax.plot([0, 0], [0, ymax], 'k', zorder=11)
+y_zero = [0 for val in x]
+ax.fill_between([start, 0], [ymax, ymax], y2=[0, 0], color='k', zorder=12, alpha=0.2)
+plt.ylim(0, ymax)
+plt.xlim(start, stop)
+ax.plot(x, dist, 'b')
+ax.plot(x, dist2, 'r')
+ax.fill_between(x, dist, color='b', y2=y_zero, zorder=9, alpha=0.2)
+ax.fill_between(x, dist2, color='r', y2=y_zero, zorder=10, alpha=0.2)
+plt.show()
+
+sides = 10
+max_size = 150
+
+one_die = [0]
+one_die.extend([1.0/sides]*sides)
+one_die.extend([0]*(max_size-sides-1))
+print(one_die)
+dice_dist = [one_die]
+
+def comb_dist(dist_one, dist_two):
+    summed = [0]*max_size
+    for i in range(len(dist_one)):
+        for j in range(len(dist_two)):
+            prob = dist_one[i]*dist_two[j]
+            if prob > 0:
+                sum = i + j
+                summed[sum] += prob
+    return summed
+
+for i in range(10):
+    dice_dist.append(comb_dist(dice_dist[i], one_die))
+    plt.plot(dice_dist[i])
 plt.show()
